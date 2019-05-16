8fab51bccfdb564ef44631aee646b2599950e9ed
Marija <mari4w04@stud.kea.dk>
2019-05-16 09:35:05 +0200

    Some changes

--- profile.php
+++ profile.php
@@ -18,10 +18,19 @@
     // }
 
 
-    $sLinktoExtraCss = '<link rel="stylesheet" href="css/white-theme.css">';
+    $sLinktoExtraCss = '<link rel="stylesheet" href="css/white-theme.css"><link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />';
     $sProfileLink = 'profile';
     $sSettingsLink = 'settings';
+    $sGreyBodyClass = "class='grey-bg'";
     require_once 'top-logged-in.php';
-    
-    
 ?>
+<div class="container contents-centered">
+        <div class="card ">
+            <input type="text" name="daterange" value="01/01/2018 - 01/15/2018" />
+        </div>
+</div>
+
+
+<?php 
+$sLinktoScript = '<script src="js/profile.js"></script>';
+require_once 'bottom.php'; ?>
