bd6ef63b4b5d08809ddc401524f895c38286d9a3
Ioana Vládáu-Babii <ioan0414@stud.kea.dk>
2019-05-20 15:55:48 +0200

    upload photos dogsitters without duplication

--- upload-dogsitter-photos.php
+++ upload-dogsitter-photos.php
@@ -2,21 +2,64 @@
 
 require_once __DIR__.'/connect.php';
 
-$dogSitterPicturesUploaded = isset($_FILES['dogSitterPhotos']) ? true : false;
+// $dogSitterPicturesUploaded = isset($_FILES['dogSitterPhotos']) ? true : false;
+// echo $dogSitterPicturesUploaded;
 
-  if($dogSitterPicturesUploaded){
-    foreach ($_FILES['dogSitterPhotos']['name'] as $key => $name){
-      $newFilename = $_FILES['dogSitterPhotos']['name'][$key];
-      $target_file = 'images/dog-sitter-photos/' . basename($newFilename);
-      move_uploaded_file($_FILES['dogSitterPhotos']['name'][$key], $target_file);
-      // $image = '../img/' . $newFilename;
+// $target_dir_dog_sitters = "images/dog-sitter-photos/";
 
-      $stmt = $db->prepare('INSERT INTO dog_sitters_photos(user_fk, url) VALUES (:userId, :newFileName)');
-      // $stmt->bindValue(':profilePhotoUrl', $target_file);
-      $stmt->bindValue(':newFileName', $newFilename);
-      $stmt->bindValue(':userId', $userId);
-      $stmt->execute();
-    }
+  // DOG SITTERS PHOTOS UPLOAD FOR DOG SITTERS
+
+    // foreach ($_FILES['dogSitterPhotos']['name'] as $key => $name){
+    //   $newFilename = $_FILES['dogSitterPhotos']['name'][$key];
+    //   $target_file = 'images/dog-sitter-photos/' . $newFilename;
+    //   move_uploaded_file($_FILES['dogSitterPhotos']['name'][$key], $target_file);
+    //   // $image = '../img/' . $newFilename;
+    //   if(move_uploaded_file($_FILES['dogSitterPhotos']['name'][$key], $target_file)){
+    //     $stmt = $db->prepare('INSERT INTO dog_sitters_photos(user_fk, url) VALUES (:userId, :newFileName)');
+    //     // $stmt->bindValue(':profilePhotoUrl', $target_file);
+    //     $stmt->bindValue(':newFileName', $newFilename);
+    //     $stmt->bindValue(':userId', $userId);
+    //     $stmt->execute();
+    //   }
+    // }
+
+    if (isset($_FILES['dogSitterPhotos'])) {
+      foreach ($_FILES['dogSitterPhotos']['name'] as $key => $name){
+          $allowedExts = array("gif", "jpeg", "jpg", "png");
+          $temp = explode(".", $_FILES["dogSitterPhotos"]["name"][$key]);
+          $extension = end($temp);
+          if ((($_FILES["dogSitterPhotos"]["type"][$key] == "image/gif")
+                  || ($_FILES["dogSitterPhotos"]["type"][$key] == "image/jpeg")
+                  || ($_FILES["dogSitterPhotos"]["type"][$key] == "image/jpg")
+                  || ($_FILES["dogSitterPhotos"]["type"][$key] == "image/pjpeg")
+                  || ($_FILES["dogSitterPhotos"]["type"][$key] == "image/x-png")
+                  || ($_FILES["dogSitterPhotos"]["type"][$key] == "image/png"))
+              && ($_FILES["dogSitterPhotos"]["type"][$key] < 500000)
+              && in_array($extension, $allowedExts)
+          ) {
+              if ($_FILES["dogSitterPhotos"]["error"][$key] > 0) {
+                  echo "Return Code: " . $_FILES["dogSitterPhotos"]["error"][$key] . "<br>";
+              } else {
+                $tmp = explode(".", $_FILES["dogSitterPhotos"]["name"][$key]);
+                $ext = end($tmp);
+                $filename = current(explode(".", $_FILES["dogSitterPhotos"]["name"][$key]));
+                $newname = $filename . '.' . $ext;
+                move_uploaded_file($_FILES["dogSitterPhotos"]["tmp_name"][$key],
+                    "images/dog-sitter-photos/" . $newname);
+                  // move_uploaded_file($_FILES["dogSitterPhotos"]["tmp_name"][$key],
+                  //     "images/dog-sitter-photos/" . $newname);
+
+                      $stmt = $db->prepare('INSERT IGNORE INTO dog_sitters_photos(user_fk, url) VALUES (:userId, :newFileName)');
+                          // $stmt->bindValue(':profilePhotoUrl', $target_file);
+                      $stmt->bindValue(':newFileName', $newname);
+                      $stmt->bindValue(':userId', $userId);
+                      $stmt->execute();
+              }
+          } else {
+              echo "<div class='alert alert-success'>Image type or size is not valid.</div>";
+          }
+
+        }
   }
     
 ?>
