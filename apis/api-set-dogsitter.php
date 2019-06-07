ff58bf5e401a8b84be7f66a4502aa35a4643ffd9
Ioana <ioanav3796@gmail.com>
2019-06-07 13:56:44 +0200

    skills saving to dog_sitters_skills when i become a dog sitter

--- apis/api-set-dogsitter.php
+++ apis/api-set-dogsitter.php
@@ -12,8 +12,46 @@ if( !isset($_GET["txtiFareDogSitter"]) && !isset($_GET["txtsAboutDogSitter"])){
         $sAboutDogSitter = $_GET['txtsAboutDogSitter'];
 }
 
-try{
+if(!isset($_GET['rbVetStudies'])){
+    sendResponse(0,__LINE__,"Please select if you have vet studies");
+  } else {
+      $bVetStudies = $_GET['rbVetStudies'];
+      if($bVetStudies == 'yes'){
+        $bVetStudies = 1; 
+      } else $bVetStudies = 0; 
+  }
 
+  if(!isset($_GET['rbOralMedication'])){
+    sendResponse(0,__LINE__,"Please select if you can give dogs oral medication");
+  } else {
+      $bOralMedication = $_GET['rbOralMedication'];
+      if($bOralMedication == 'yes'){
+        $bOralMedication = 1; 
+      } else $bOralMedication = 0; 
+  }
+
+  if(!isset($_GET['rbVaccinateDogs'])){
+    sendResponse(0,__LINE__,"Please select if you are able to vaccinate dogs");
+  } else {
+      $bVaccinateDogs = $_GET['rbVaccinateDogs'];
+      if($bVaccinateDogs == 'yes'){
+        $bVaccinateDogs = 1; 
+      } else $bVaccinateDogs = 0; 
+  }
+
+  if(!isset($_GET['rbTrainingTechniques'])){
+    sendResponse(0,__LINE__,"Please select if you are familiar with training techniques");
+  } else {
+      $bTrainingTechniques = $_GET['rbTrainingTechniques'];
+      if($bTrainingTechniques == 'yes'){
+        $bTrainingTechniques = 1; 
+      } else $bTrainingTechniques = 0; 
+  }
+
+  // id skills table: 1            2                   3                   4
+  $aSkills = [$bVetStudies, $bOralMedication, $bVaccinateDogs, $bTrainingTechniques];
+
+try{
     $db->beginTransaction(); // all or nothing
 
     $stmt0 = $db->prepare('SELECT * FROM users WHERE email = :sEmail');
@@ -34,22 +72,51 @@ try{
         $stmt2 = $db->prepare("UPDATE users SET is_dog_sitter = 1 WHERE id = :sUserId");
         $stmt2->bindValue(':sUserId', $sUserId);
         $stmt2->execute();
+        
     }
+
+
+    foreach($aSkills as $key=>$skill){
+        // if($aSkills[$i] == 1){
+            // echo $key;
+        $key++;
+        if($skill == 1){
+            $stmt3 = $db->prepare("INSERT into dog_sitters_skills VALUES(:sUserId, :iSkillFk)");
+            $stmt3->bindValue(':sUserId', $sUserId);
+            $stmt3->bindParam(':iSkillFk', $key);
+            $stmt3->execute();
+        }
+
+
+        // if( !$stmt3->execute() ){
+        //     $db->rollBack();
+        //     //    echo 'Cannot insert the dog sitter '.__LINE__;
+        //         sendResponse(0, __LINE__, "Cannot save dog sitter to DB, error with skills");
+        //     //    exit;
+        // }
+    }
+
+
+    
+
 
 
    if( !$stmt0->execute() ){
        $db->rollBack();
     //    echo 'Cannot insert the dog sitter '.__LINE__;
-       sendResponse(0, __LINE__, "Cannot save dog sitter to DB");
+       sendResponse(0, __LINE__, "Cannot save dog sitter to DB, error with inserting user into dog_sitters");
     //    exit;
    }
 
    if( !$stmt2->execute() ){
     //    echo 'Cannot update is_dog_sitter status '.__LINE__;
-       sendResponse(0, __LINE__, "Cannot save dog sitter to DB");
+       sendResponse(0, __LINE__, "Cannot save dog sitter to DB, error with setting is_dog_sitter to 1");
        $db->rollBack();
     //    exit;
    }
+
+
+
 
    $db->commit(); 
    sendResponse(1, __LINE__, "Success, dog sitter saved to DB");
--- become-a-dogsitter.php
+++ become-a-dogsitter.php
@@ -45,9 +45,42 @@
                 <label for="txtAbout">About Me</label>
                 <textarea name="txtsAboutDogSitter" id="txtsAboutDogSitter"></textarea><br>
             </div>
+
+            <label for="rbVetStudies">I have studied in vet school*</label>
+            <div class="dog-gender">
+                <input type="radio" name="rbVetStudies" value="yes" class="radio" <?php if (isset($_POST['rbVetStudies']) && $_POST['rbVetStudies'] == 'Yes'): ?>checked='checked'<?php endif; ?> /> <span>Yes</span>
+                <input type="radio" name="rbVetStudies" value="no"  class="radio" <?php if (isset($_POST['rbVetStudies']) && $_POST['rbVetStudies'] ==  'No'): ?>checked='checked'<?php endif; ?> /> No <br>
+            </div>
+
+
+            <label for="rbOralMedication">I can manage oral medication for dogs*</label>
+            <div class="dog-gender">
+                <input type="radio" name="rbOralMedication" value="yes" class="radio" <?php if (isset($_POST['rbOralMedication']) && $_POST['rbOralMedication'] == 'Yes'): ?>checked='checked'<?php endif; ?> /> <span>Yes</span>
+                <input type="radio" name="rbOralMedication" value="no"  class="radio" <?php if (isset($_POST['rbOralMedication']) && $_POST['rbOralMedication'] ==  'No'): ?>checked='checked'<?php endif; ?> /> No <br>
+            </div>
+
+            <label for="rbVaccinateDogs">I know how to vaccinate dogs*</label>
+            <div class="dog-gender">
+                <input type="radio" name="rbVaccinateDogs" value="yes" class="radio" <?php if (isset($_POST['rbVaccinateDogs']) && $_POST['rbVaccinateDogs'] == 'Yes'): ?>checked='checked'<?php endif; ?> /> <span>Yes</span>
+                <input type="radio" name="rbVaccinateDogs" value="no"  class="radio" <?php if (isset($_POST['rbVaccinateDogs']) && $_POST['rbVaccinateDogs'] ==  'No'): ?>checked='checked'<?php endif; ?> /> No <br>
+            </div>
+
+            <label for="rbTrainingTechniques">I am familiar with dog training techniques*</label>
+            <div class="dog-gender">
+                <input type="radio" name="rbTrainingTechniques" value="yes" class="radio" <?php if (isset($_POST['rbTrainingTechniques']) && $_POST['rbTrainingTechniques'] == 'Yes'): ?>checked='checked'<?php endif; ?> /> <span>Yes</span>
+                <input type="radio" name="rbTrainingTechniques" value="no"  class="radio" <?php if (isset($_POST['rbTrainingTechniques']) && $_POST['rbTrainingTechniques'] ==  'No'): ?>checked='checked'<?php endif; ?> /> No <br>
+            </div>
+
+            
+
+
+
             <input type="file" name="dogSitterPicturesToUpload" id="dogSitterPicturesToUpload" class="custom-file-input custom-file-input-one-file">
             <input class="yellow-btn" type="submit" value="Become a dogsitter!" name="submit">
             
+
+            </div>
+
         </form>
     </div>
 </div>
